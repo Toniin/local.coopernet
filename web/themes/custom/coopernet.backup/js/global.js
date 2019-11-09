466b0fdf3a352970c9a95ef73ad2c989396365ba
Toniin <antonin.chaudiere@hotmail.fr>
2019-11-09 11:57:18 +0100

    Initialisation of Drupal project: local.Coopernet.

---
+++ web/themes/custom/coopernet.backup/js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.coopernet = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
